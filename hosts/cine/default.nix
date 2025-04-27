f28d7f7232094e6337c2406e335b2cd9f4b5a93d
Linos Giannopoulos <linosgian00@gmail.com>
2025-04-27 20:07:05 +0300

    hosts::cine: refactor the main nix file

--- hosts/cine/default.nix
+++ hosts/cine/default.nix
@@ -8,49 +8,35 @@
 {
   imports = [
     ../../blueprints/server.nix
-    (modulesPath + "/profiles/qemu-guest.nix")
+    ./hardware-configuration.nix
   ];
-  services.qemuGuest.enable = true;
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    autoResize = true;
-    fsType = "ext4";
-  };
-
-  boot.loader.grub.device = lib.mkDefault "/dev/vda";
-  boot.kernelParams = [
-    "console=ttyS0,115200"
-    "console=tty1" # enables libvirt console
-  ];
-  boot.growPartition = true;
-
-  boot.kernelModules = [ "virtiofs" ];
-
-  fileSystems."/media" = {
-    device = "media";
-    fsType = "virtiofs";
-    options = [ "defaults" "nofail" ];
-  };
-
-  system.build.qcow2 = import "${modulesPath}/../lib/make-disk-image.nix" {
-    inherit lib config pkgs;
-    diskSize = 10240;
-    format = "qcow2";
-    partitionTableType = "hybrid";
-  };
-
   networking.nameservers = ["1.1.1.1"];
 
   networking.hostName = "cine";
   networking.useDHCP = lib.mkDefault true;
-  networking.firewall.interfaces."enp0s3".allowedTCPPorts = [ 22 9000 9999 ];
-  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 9000 9999 ];
+  networking.firewall.interfaces."enp2s1".allowedTCPPorts = [ 22 80 9100 ];
+  networking.firewall.interfaces."enp2s1".allowedUDPPorts = [ 41641 ];
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    virtualHosts."_" = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096/";
+        proxyWebsockets = true;
+      };
+    };
+  };
 
   services.tailscale.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    minio-client
-  ];
+  services.jellyfin = {
+    enable = true;
+    dataDir = "/var/lib/jellyfin/data";
+    cacheDir = "/var/lib/jellyfin/cache";
+    configDir = "/var/lib/jellyfin/config";
+  };
 
   system.stateVersion = "24.11";
 }
---
+++ hosts/cine/hardware-configuration.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+  services.qemuGuest.enable = true;
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    autoResize = true;
+    fsType = "ext4";
+  };
+  fileSystems."/media" = {
+    device = "media";
+    fsType = "virtiofs";
+    options = [ "defaults" "nofail" ];
+  };
+  fileSystems."/ssd" = {
+    device = "/dev/vda";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
+  fileSystems."/var/lib/jellyfin" = {
+    device = "jellyfin";
+    fsType = "virtiofs";
+    options = [ "defaults" "nofail" ];
+  };
+
+  boot.loader.grub.device = lib.mkDefault "/dev/vda";
+  boot.kernelParams = [
+    "console=ttyS0,115200"
+    "console=tty1" # enables libvirt console
+  ];
+  boot.growPartition = true;
+
+  boot.kernelModules = [ "virtiofs" ];
+
+  system.build.qcow2 = import "${modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    diskSize = 10240;
+    format = "qcow2";
+    partitionTableType = "hybrid";
+  };
+}
