e5f0f8dbf258f574d7ef10c5a8a81e4817949286
Linos Giannopoulos <linosgian00@gmail.com>
2025-09-09 09:38:13 +0300

    hosts::headscale: Hijack host for exit node VPN

--- hosts/headscale/default.nix
+++ hosts/headscale/default.nix
@@ -3,16 +3,14 @@
   imports = [
     ../../blueprints/server.nix
     ./hardware-configuration.nix
-    ../../modules/tailhub
-    ../../modules/headscale
-    {
-      headscale.hostname = "headscale.lgian.com";
-      headscale.v4Prefix = "198.18.0.0/24";
-    }
   ];
   networking.hostName = "headscale";
 
-  networking.firewall.allowedTCPPorts = [ 22 80 443 9000 ];
+  services.tailscale.enable = true;
+  boot.kernel.sysctl."net.ipv4.conf.all.forwarding" = true;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedUDPPorts = [ 41641 ];
 
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 }
