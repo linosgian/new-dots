b444a4ff191c3d1836db8f41e97c2022c328861a
Linos Giannopoulos <linosgian00@gmail.com>
2026-03-27 15:36:09 +0200

    oldrouter: Prepare for tilda

--- flake.nix
+++ flake.nix
@@ -223,15 +223,14 @@
           openwrt-imagebuilder.lib.build config;
         oldrouter =
           let
-            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            pkgs = nixpkgs-master.legacyPackages.x86_64-linux;
 
             profiles = openwrt-imagebuilder.lib.profiles {
               inherit pkgs;
-              release = "24.10.2";
+              release = "25.12.0";
             };
 
             config = profiles.identifyProfile "xiaomi_redmi-router-ax6s" // {
-              disabledServices = [ "dnsmasq" ];
               packages = [
                 "-odhcpd-ipv6only"
                 "odhcpd"
@@ -241,8 +240,6 @@
                 "bind-dig"
                 "coreutils"
                 "curl"
-                "https-dns-proxy"
-                "ddns-scripts-digitalocean"
                 "openssh-server"
                 "openssh-sftp-server"
                 "openssh-client"
@@ -255,32 +252,18 @@
                 "irqbalance"
                 "adblock"
                 "luci-ssl"
-                "acme-acmesh"
-                "acme-acmesh-dnsapi"
                 "zsh"
                 "vim-fuller"
                 "netcat"
-                "unbound-control"
-                "unbound-daemon"
                 "lm-sensors"
                 "lua-cjson"
                 "prometheus-node-exporter-lua"
-                "prometheus-node-exporter-lua-sqm"
                 "prometheus-node-exporter-lua-openwrt"
                 "prometheus-node-exporter-lua-wifi"
                 "prometheus-node-exporter-lua-wifi_stations"
                 "qosify"
                 "ss"
-                "tc-full"
-                "unbound_exporter"
-                "smokeping_prober"
               ];
-              files = pkgs.runCommand "image-files" { } ''
-                mkdir -p $out/etc/uci-defaults
-                  cat > $out/etc/uci-defaults/99-custom <<EOF
-                  sed -i '/\s*devices = {{ fw4\.set(flowtable_devices, true) }};/s/{{.*}}/{ "eth1", "lan1", "lan2", "lan3", "lan4", "phy0-ap0", "phy0-ap1", "phy1-ap0", "phy1-ap1" }/' /usr/share/firewall4/templates/ruleset.uc
-                  EOF
-              '';
             };
           in
           openwrt-imagebuilder.lib.build config;
