dce75008a46e502a780755f48114274264ad04a6
Linos Giannopoulos <linosgian00@gmail.com>
2026-07-08 18:42:15 +0300

    zigbee: Prevent prometheus-mqtt-exporter from failing on boot

--- modules/zigbee/default.nix
+++ modules/zigbee/default.nix
@@ -86,6 +86,18 @@
       "--prometheus-prefix mqtt_"
     ];
   };
+  systemd.services.prometheus-mqtt-exporter = {
+    after = [ "mosquitto.service" ];
+    wants = [ "mosquitto.service" ];
+
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+
+    startLimitIntervalSec = 300;
+    startLimitBurst = 20;
+  };
   services.zigbee2mqtt = {
     enable = true;
     settings = {
