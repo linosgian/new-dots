edb4a0e3374b5e7eee7c4027f699472a9a4659f1
Linos Giannopoulos <linosgian00@gmail.com>
2026-08-19 17:30:13 +0300

    modules::audiobookshelf: Fix audiobookshelf weird caching

--- modules/services/audiobookshelf.nix
+++ modules/services/audiobookshelf.nix
@@ -8,6 +8,12 @@ in
     port = cfg.defs.audiobookshelf.port;
   };
 
+  services.nginx.virtualHosts."podcasts".locations."/".extraConfig = ''
+    proxy_hide_header ETag;
+    proxy_hide_header Last-Modified;
+    proxy_hide_header Cache-Control;
+    add_header Cache-Control "no-store";
+  '';
   systemd.services.audiobookshelf.serviceConfig = {
     WorkingDirectory = lib.mkForce "/ssd-new/audiobookshelf";
   };
